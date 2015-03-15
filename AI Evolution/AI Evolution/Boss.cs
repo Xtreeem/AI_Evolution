c8979c9a4a8aeb04bd43ffbe747cdd7af625e654
Gourty <andy19931@hotmail.com>
2015-03-15 19:55:27 +0100

    Boss thingy

---
+++ AI Evolution/AI Evolution/Boss.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AI_Evolution
+{
+    class Boss : Hero
+    {
+
+
+        public Boss(StatWeight StatWeight, float TotalStats)
+            : base(StatWeight, TotalStats)
+        {
+
+        }
+    }
+}
